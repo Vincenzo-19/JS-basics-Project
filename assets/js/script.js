dde12693af432129528855ab90a8959cd27c8d32
VincenzoNurcato <nurcatov@gmail.com>
2024-06-21 20:22:01 +0200

    migliorata struttura html + stile CSS terminato + Javascript Completato

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,66 @@
-const TITLE = document.querySelector('h1')
 const BUTTONS_CONTAINER = document.querySelector('.buttonsContainer')
 
-// creation of result
+
+
+// creation and styling of decrement Button
+const DECREMENT_BTN = document.createElement('button')
+DECREMENT_BTN.textContent = '-'
+
+DECREMENT_BTN.className = 'decrement-button btn btn-outline-danger me-4'
+
+BUTTONS_CONTAINER.append(DECREMENT_BTN)
+
+// creation and styling of reset Button
+const RESET_BTN = document.createElement('button')
+RESET_BTN.textContent = 'Reset'
+
+RESET_BTN.className = 'reset-button btn btn-outline-danger'
+
+BUTTONS_CONTAINER.append(RESET_BTN)
+
+// creation and styling of increment Button
+const INCREMENT_BTN = document.createElement('button')
+INCREMENT_BTN.textContent = '+'
+
+INCREMENT_BTN.className = 'increment-button btn btn-outline-danger ms-4'
+
+BUTTONS_CONTAINER.append(INCREMENT_BTN)
+
+// cresation of result
+
 let result = document.createElement('div')
-result.textContent = 0
-TITLE.after(result)
+result.textContent = '0'
 
-result.style.cssText = `text-align: center;
-color: yellowgreen;
-font-size: 20px;
-`
+result.className = 'result-value fs-3 mt-4 mb-4'
 
-// creation and styling of decrementButton
+document.querySelector('h1').after(result)
 
-let decrementButton = document.createElement('button')
-decrementButton.textContent = '-'
-BUTTONS_CONTAINER.appendChild(decrementButton)
-decrementButton.style.cssText = `padding: 10px;
-margin: 20px;
-background-color: black;
-color: white;
-border-radius: 15px
-`
+// 
+// 
+// 
 
-// creation and styling of resetButton
+const DECREMENT_BUTTON = document.querySelector('.decrement-button')
+const RESET_BUTTON = document.querySelector('.reset-button')
+const INCREMENT_BUTTON = document.querySelector('.increment-button')
+let resultValue = document.querySelector('.result-value')
 
-let resetButton = document.createElement('button')
-resetButton.textContent = 'Reset'
-BUTTONS_CONTAINER.appendChild(resetButton)
-resetButton.style.cssText = `padding: 10px;
-margin: 20px;
-background-color: black;
-color: white;
-border-radius: 15px
-`
+let currentValue = 0;
 
+INCREMENT_BTN.addEventListener('click', () => {
+    currentValue += 1
+    resultValue.innerHTML = currentValue
+})
 
-// creation and styling of incrementButton
+RESET_BTN.addEventListener('click', () => {
+    currentValue = 0
+    resultValue.innerHTML = currentValue
+})
 
-let incrementButton = document.createElement('button')
-incrementButton.textContent = '+'
-BUTTONS_CONTAINER.appendChild(incrementButton)
-incrementButton.style.cssText = `padding: 10px;
-margin: 20px;
-background-color: black;
-color: white;
-border-radius: 15px
-`
+DECREMENT_BTN.addEventListener('click', () => {
+    currentValue -= 1
+    resultValue.innerHTML = currentValue
+})
+
 
 
 
